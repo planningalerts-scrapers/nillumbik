d0a3afa2c4858887d7d6b2c283ebaf19195d01f3
Matthew Landauer <mlandauer@gmail.com>
2019-06-01 11:00:42 +1000

    Update to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,6 @@
 require "epathway_scraper"
 
-EpathwayScraper::Scraper.scrape_and_save(
+EpathwayScraper.scrape_and_save(
   "https://epathway.nillumbik.vic.gov.au/ePathway/Production",
   list_type: :advertising
 )
