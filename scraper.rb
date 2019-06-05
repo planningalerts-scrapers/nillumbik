519e26685a0e59b5561f555fdc5e9eb363ba6df5
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 12:05:50 +1000

    Upgrade to latest version of the gem and adds missing state to the address

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,5 @@ require "epathway_scraper"
 
 EpathwayScraper.scrape_and_save(
   "https://epathway.nillumbik.vic.gov.au/ePathway/Production",
-  list_type: :advertising
+  list_type: :advertising, state: "VIC"
 )
